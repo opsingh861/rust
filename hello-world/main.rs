929c526cb5d725f075495d4510ddbecf4ab35fd3
Aditya Dhanraj <opsingh861@gmail.com>
2025-03-15 12:47:04 +0530

    loops

--- hello-world/main.rs
+++ hello-world/main.rs
@@ -1,10 +1,28 @@
 fn main() {
-    // conditionals
-    let x = 5;
+    // Loop
+    for i in 0..5 {
+        // 0, 1, 2, 3, 4
+        println!("i is = {}", i);
+    }
 
-    if x % 2 == 0 {
-        println!("x is even");
-    } else {
-        println!("x is odd");
+    println!("Looping through string: ");
+
+    let word = "Hello, World!";
+    for c in word.chars() {
+        // H, e, l, l, o, ,,  , W, o, r, l, d, !
+        println!("c is = {}", c);
+    }
+
+    println!("Looping through string: ");
+    let word1 = String::from("World!");
+    for c in word1.chars() {
+        // H, e, l, l, o, ,,  , W, o, r, l, d, !
+        println!("c is = {}", c);
+    }
+
+    let mut i = 0;
+    while i < 100 {
+        i += 1;
+        println!("i is = {}", i);
     }
 }
