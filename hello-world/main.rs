7da9f40a84a954636e2d136672fca5b4d5ee84a1
Aditya Dhanraj <opsingh861@gmail.com>
2025-03-15 12:52:46 +0530

    functions

--- hello-world/main.rs
+++ hello-world/main.rs
@@ -1,28 +1,8 @@
 fn main() {
-    // Loop
-    for i in 0..5 {
-        // 0, 1, 2, 3, 4
-        println!("i is = {}", i);
-    }
+    // functions
+    println!("The sum of 5 and 6 is {}", sum(5, 6));
+}
 
-    println!("Looping through string: ");
-
-    let word = "Hello, World!";
-    for c in word.chars() {
-        // H, e, l, l, o, ,,  , W, o, r, l, d, !
-        println!("c is = {}", c);
-    }
-
-    println!("Looping through string: ");
-    let word1 = String::from("World!");
-    for c in word1.chars() {
-        // H, e, l, l, o, ,,  , W, o, r, l, d, !
-        println!("c is = {}", c);
-    }
-
-    let mut i = 0;
-    while i < 100 {
-        i += 1;
-        println!("i is = {}", i);
-    }
+fn sum(a: i32, b: i32) -> i32 { // return type using ->
+    a + b
 }
