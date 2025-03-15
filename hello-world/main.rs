a0f21952155947cdfa2ea1c79cce1a0c3453eab6
Aditya Dhanraj <opsingh861@gmail.com>
2025-03-15 22:30:30 +0530

    Stack and Heap

--- hello-world/main.rs
+++ hello-world/main.rs
@@ -1,10 +1,15 @@
 fn main() {
-    // Variables are immutable by default in Rust
-    // let x = 5; // it is immutable
-    // x = 6; // error: cannot assign twice to immutable variable `x`
+    // heap and stacks
+    let x: i32 = 7; // this will go to stack, as at the compile time we know the size so need to increase the size in near future
+    // stack is fast, whenever it will go out of scope it will be deleted.
 
-    let mut x = 5; // it is mutable
-    println!("The value of x is: {}", x);
-    x = 6; // no error because x is mutable
-    println!("The value of x is: {}", x);
+    let greeting = String::from("Hey there"); // In case of string we are not sure of size so it will be stored in the heap and the pointer will be stored in the stack
+    // heap is slow, but we can store large data in it.
+    // stack will store the pointer to the heap memory, length and capacity of the string.
+    print!(
+        "Capacity: {}   Length: {}   Pointer: {:p}\n",
+        greeting.capacity(),
+        greeting.len(),
+        greeting.as_ptr()
+    );
 }
