be32b244b99152a579c7aebc626cb6dd7735ce42
Aditya Dhanraj <opsingh861@gmail.com>
2025-03-16 00:13:46 +0530

    Borrowing

--- hello-world/main.rs
+++ hello-world/main.rs
@@ -1,32 +1,37 @@
 fn main() {
-    // Ownership
+    // Borrowing
     let s1 = String::from("hello");
-    let s2 = s1;
-    // println!("{}, world!", s1); // This will not work because s1 has been moved to s2
-    println!("{}, world!", s2);
+    let s2 = &s1;
+    let s3 = &s1; // we can have multiple immutable references to the same value
+    let len = calculate_length(&s1);
+    println!("The length of '{}' is {}.", s1, len);
 
-    let some_string = String::from("hello");
-    transfer_ownership(some_string);
-    // println!("{}", some_string); // This will not work because some_string has been moved to transfer_ownership
+    // Mutable references
+    let something = String::from("hello");
+    wrong_change(&something); // This will not compile because s is borrowed as immutable
+    wrong_change(&mut something); // this will also throw an error because we are trying to borrow the value as mutable which is immutable in nature
 
-    let mut one_more_string = String::from("hello");
-    one_more_string = take_and_give_back(one_more_string);
-    println!("{}", one_more_string); // This will work because one_more_string has been moved to take_and_give_back and then moved back to one_more_string
+    let mut s = String::from("hello");
+    change(&mut s);
+    println!("{}", s);
 
-    // Clone
-    let s3 = String::from("hello");
-    let s4 = s3.clone();
-    println!("s3 = {}, s4 = {}", s3, s4); // This will work because s3 has been cloned to s4
-
-    let s5 = String::from("hello");
-    transfer_ownership(s5.clone());
-    println!("{}", s5); // This will work because s5 has been cloned to transfer_ownership
+    let mut r1 = String::from("hello");
+    let r2 = &mut r1;
+    let r3 = &mut r1; // We cannot have multiple mutable references to the same value but there is catch, if we are not using the first mutable reference then we can have mutable reference to the same value
+    let r4 = &r1; // It will also throw error because after trying borrowing anything as mutable reference we can't even borrow it as immutable reference
+    print!("{}", r1); // Till now it will work
+    print!("{}", r2); // Till now it will work as we are we are using r2 which was which was borrowed as mutable reference
+    print!("{}", r3); // This will throw an error because we can't have multiple mutable references to the same value
 }
 
-fn transfer_ownership(some_string: String) {
-    println!("{}", some_string); // This will work because some_string has been moved to transfer_ownership
+fn calculate_length(s: &String) -> i32 {
+    return s.len() as i32;
 }
 
-fn take_and_give_back(a_string: String) -> String {
-    return a_string;
+fn wrong_change(something: &String) {
+    something.push_str(", world"); // we are trying to modify the borrowed value which is not mutable in nature
+}
+
+fn change(s: &mut String) {
+    s.push_str(", world");
 }
