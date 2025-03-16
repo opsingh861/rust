460ce09727d23141df26f6d3a0da249c4c14d022
Aditya Dhanraj <opsingh861@gmail.com>
2025-03-16 11:33:49 +0530

    External packages

--- main.rs
+++ main.rs
@@ -1,24 +1,7 @@
-// Options are an enum type that is defined in the standard library, since there is not concept of null here so we can use option to return None when we want to return nothing.
-
-// pub enum Option<T> { // Option is a generic enum type with one type parameter
-//     None,
-//     Some(T),
-// }
+// External crate or package ( cargo add package_name )
+use rand::random;
 
 fn main() {
-    let my_name = String::from("Rust");
-    let result = first_a_index(&my_name);
-    match result {
-        Some(index) => println!("The first 'a' in 'Rust' is at index: {}", index),
-        None => println!("There is no 'a' in {}", my_name),
-    }
-}
-
-fn first_a_index(s: &str) -> Option<usize> {
-    for (i, c) in s.chars().enumerate() {
-        if c == 'a' {
-            return Some(i);
-        }
-    }
-    None
+    let random_number = random::<u128>();
+    println!("Random number: {}", random_number);
 }
