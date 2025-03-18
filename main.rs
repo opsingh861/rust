15c24900d276555b30d37f9b0aa0653bae0e0def
Aditya Dhanraj <opsingh861@gmail.com>
2025-03-18 08:35:28 +0530

    Lifetimes

--- main.rs
+++ main.rs
@@ -1,66 +1,31 @@
-// traits
-
-use std::iter::Sum;
-
-pub trait Summary {
-    // fn summarize(&self) -> String;
-
-    fn summarize(&self) -> String {
-        // self parameter should be there
-        // we can define default implementation also, if we implement it again at other place so it will overridden
-        return format!("This is default summarize");
-    }
-}
-
-pub trait Fix {
-    fn console(&self) {
-        println!("This is from Fix");
-    }
-}
-
-struct User {
-    name: String,
-    age: i32,
-}
-
-// impl Summary for User {
-//     fn summarize(&self) -> String {
-//         return format!("My name is {} and my age is {}", self.name, self.age);
-//     }
-// }
-
-impl Summary for User {}
-// can implement more than one
-impl Fix for User {}
+// Lifetime
 
 fn main() {
-    let user = User {
-        name: String::from("Aditya Dhanraj"),
-        age: 24,
-    };
+    let ans: &str;
 
-    // println!("{}", user.summarize());
+    let s1 = String::from("small");
+    {
+        let s2 = String::from("longer");
+        ans = longest(&s1, &s2); // it will not compile as lifetime of s2 is not long enough
+    }
 
-    notify(user);
+    println!("{}", ans); // if i will not access this line it will work, but here we are trying to access ans which can point to s2 which will be out of scope before this line.
 }
 
-// fn notify(u: impl Summary) {
-//     // u should implement Summary trait
-//     println!("{}", u.summarize())
+// fn longest(s1: &str, s2: &str) -> String {
+//     // this will work but when we will change the return type to &str, the problem will start
+//     if s1.len() > s2.len() {
+//         return s1.to_string();
+//     }
+
+//     return s2.to_string();
 // }
 
-// pub fn nofify2<T: Summary>(u: T) {
-//     // this is same as above, above is syntexial sugar
-//     println!("{}", u.summarize())
-// }
+fn longest<'a>(s1: &'a str, s2: &'a str) -> &'a str {
+    // needs to give lifetime specifier
+    if s1.len() > s2.len() {
+        return s1;
+    }
 
-// fn notify(u: impl Summary + Fix) {
-//     u.console();
-//     u.summarize();
-// }
-
-pub fn notify<T: Summary + Fix>(u: T) {
-    // both are same the above one and this one
-    u.console();
-    u.summarize();
+    return s2;
 }
