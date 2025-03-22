eeb9c73eaae65a76a272d25d1ec4c593a260e2e8
Aditya Dhanraj <opsingh861@gmail.com>
2025-03-22 13:04:07 +0530

    async using tokio

--- main.rs
+++ main.rs
@@ -1,13 +1,22 @@
-// message passing
+use std::time::Duration;
+use tokio::time::sleep;
 
-use std::{sync::mpsc, thread::spawn};
+async fn task_one() {
+    sleep(Duration::from_secs(2)).await;
+    println!("Task One Done!");
+}
 
-fn main() {
-    let (tx, rx) = mpsc::channel();
-    spawn(move || {
-        tx.send(String::from("hello world")).unwrap();
-    });
+async fn task_two() {
+    sleep(Duration::from_secs(1)).await;
+    println!("Task Two Done!");
+}
 
-    let value = rx.recv().unwrap();
-    println!("{}", value);
+#[tokio::main]
+async fn main() {
+    println!("Starting tasks...");
+
+    // Run both tasks concurrently
+    let (res1, res2) = tokio::join!(task_one(), task_two());
+
+    println!("Both tasks finished!");
 }
