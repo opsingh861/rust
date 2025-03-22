57910757cfe96eb2f833907eea9a08c74a986589
Aditya Dhanraj <opsingh861@gmail.com>
2025-03-22 12:24:08 +0530

    Messgae passing

--- main.rs
+++ main.rs
@@ -1,36 +1,13 @@
-// Multithreading for faster execution
+// message passing
 
-use std::thread;
+use std::{sync::mpsc, thread::spawn};
 
 fn main() {
-    let num_threads = 10; // Use 10 threads to aim for 10x speedup
-    let total = 100_000_000; // Total numbers to print
-    let chunk_size = total / num_threads; // Divide work among threads
+    let (tx, rx) = mpsc::channel();
+    spawn(move || {
+        tx.send(String::from("hello world")).unwrap();
+    });
 
-    // Store all thread handles
-    let mut handles = vec![];
-
-    for thread_id in 0..num_threads {
-        // Define the start and end range for each thread
-        let start = thread_id * chunk_size;
-        let end = if thread_id == num_threads - 1 {
-            total
-        } else {
-            start + chunk_size
-        };
-
-        // Spawn a thread to handle the assigned range
-        let handle = thread::spawn(move || {
-            for i in start..end {
-                println!("{}", i);
-            }
-        });
-
-        handles.push(handle);
-    }
-
-    // Wait for all threads to complete
-    for handle in handles {
-        handle.join().unwrap();
-    }
+    let value = rx.recv().unwrap();
+    println!("{}", value);
 }
